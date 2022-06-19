1aef118b808c035950ebfa22be92274d125782cd
Alexander <valverin95@gmail.com>
2022-06-19 21:03:34 +0300

    🔥️ filtering events according format date

--- src/app/calendar/components/search-event/search-event.pipe.ts
+++ src/app/calendar/components/search-event/search-event.pipe.ts
@@ -1,5 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { CalendarEvent } from '@calendar/models';
+import dayjs from 'dayjs';
+
+const format = 'D MMMM';
 
 @Pipe({
   name: 'searchEvent'
@@ -16,7 +19,8 @@ export class SearchEventPipe implements PipeTransform {
     return events.filter(event => {
       return event.title.toLowerCase().includes(search)
         || event.message?.toLowerCase().includes(search)
-        || event.people?.some(man => man.toLowerCase().includes(search));
+        || event.people?.some(man => man.toLowerCase().includes(search))
+        || dayjs(event.date).format(format).includes(search)
     });
   }
 }
